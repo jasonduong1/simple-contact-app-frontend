10db2797718a22a956551cad31dbb59c807387ce
jason <jduong89@gmail.com>
2022-04-07 14:47:37 -0700

    adds inputs for newContactParams

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      contacts: [],
+      newContactParams: {},
+    };
+  },
+  created: function () {
+    this.indexContacts();
+  },
+  methods: {
+    indexContacts: function () {
+      axios.get("http://localhost:3000/api/contacts.json").then((response) => {
+        this.contacts = response.data;
+        console.log("Contacts Index", this.contacts);
+      });
+    },
+    createContact: function () {
+      axios.post("http://localhost:3000/api/contacts.json", this.newContactParams).then((response) => {
+        this.contacts.push(response.data);
+        console.log("Contact created.", response.data);
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+
+    <div v-for="contact in contacts" :key="contact">
+      <p>{{ contact }}</p>
+    </div>
+    <div>
+      First Name:
+      <input type="text" v-model="newContactParams.first_name" />
+    </div>
+    <div>
+      Last Name:
+      <input type="text" v-model="newContactParams.last_name" />
+    </div>
+    <div>
+      Email:
+      <input type="text" v-model="newContactParams.email" />
+    </div>
+    <div>
+      Phone Number:
+      <input type="text" v-model="newContactParams.phone_number" />
+    </div>
+
+    <div>
+      <button v-on:click="createContact">Add Contact</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
