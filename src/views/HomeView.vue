0f58a941be5df19ad7888c3bc8a2023ad3b29dc9
jason <jduong89@gmail.com>
2022-04-10 22:26:41 -0700

    adds show action, update form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,10 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Contacts",
       contacts: [],
       newContactParams: {},
+      currentContact: {},
     };
   },
   created: function () {
@@ -24,6 +25,22 @@ export default {
         console.log("Contact created.", response.data);
       });
     },
+    showContact: function (contact) {
+      console.log(contact);
+      this.currentContact = contact;
+      document.querySelector("#contact-details").showModal();
+    },
+    showContactUpdate: function (contact) {
+      console.log(contact);
+      this.currentContact = contact;
+      document.querySelector("#contact-update").showModal();
+    },
+    updateContact: function (contact) {
+      let editContactParams = contact;
+      axios.patch("http://localhost:3000/api/contacts/" + contact.id + ".json", editContactParams).then((response) => {
+        console.log("Updated!", response.data);
+      });
+    },
   },
 };
 </script>
@@ -33,8 +50,42 @@ export default {
     <h1>{{ message }}</h1>
 
     <div v-for="contact in contacts" :key="contact">
-      <p>{{ contact }}</p>
+      {{ contact.first_name }} {{ contact.last_name }}
+      <button v-on:click="showContact(contact)">Info</button>
+      <button v-on:click="showContactUpdate(contact)">Update</button>
     </div>
+    <dialog id="contact-details">
+      <form method="dialog">
+        <h3>{{ currentContact.first_name }} {{ currentContact.last_name }}</h3>
+        <p>Phone: {{ currentContact.phone_number }}</p>
+        <p>Email: {{ currentContact.email }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
+    <dialog id="contact-update">
+      <form method="dialog">
+        <h3>Update contact</h3>
+        <p>
+          First name:
+          <input type="text" v-model="currentContact.first_name" />
+        </p>
+        <p>
+          Last name:
+          <input type="text" v-model="currentContact.last_name" />
+        </p>
+        <p>
+          Phone Number:
+          <input type="text" v-model="currentContact.phone_number" />
+        </p>
+        <p>
+          Email:
+          <input type="text" v-model="currentContact.email" />
+        </p>
+        <button v-on:click="updateContact(currentContact)">Update Contact</button>
+        <button>Close</button>
+      </form>
+    </dialog>
+    <p></p>
     <div>
       First Name:
       <input type="text" v-model="newContactParams.first_name" />
